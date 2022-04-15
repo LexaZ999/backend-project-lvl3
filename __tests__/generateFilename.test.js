e788f2629cacdc1a64eafd9e90aadc785712b4ca
Aleksey Zubkov <zubkovf@mail.ru>
2022-04-15 17:45:47 +0700

    Add tests for the image downloader

--- __tests__/generateFilename.test.js
+++ __tests__/generateFilename.test.js
@@ -1,8 +1,11 @@
 import generateFilename from '../src/generateFilename.js';
 
 test('generateFilename', () => {
-  const link = 'https://github.com/tj/commander.js/';
-  const expectedName = 'github-com-tj-commander-js.html';
+  const link1 = 'https://github.com/tj/commander/';
+  const expectedName1 = 'github-com-tj-commander.html';
+  expect(generateFilename(link1, '.html')).toBe(expectedName1);
 
-  expect(generateFilename(link)).toBe(expectedName);
+  const link2 = 'https://github.com/tj/commander.js/courses/pig.png';
+  const expectedName2 = 'github-com-tj-commander-js-courses-pig.png';
+  expect(generateFilename(link2)).toBe(expectedName2);
 });
--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -1,32 +1,55 @@
 import os from 'os';
-import fs from 'fs/promises';
+import fsp from 'fs/promises';
 import path from 'path';
 import nock from 'nock';
 import pageLoader from '../src/pageLoader.js';
+import getFilepath from '../src/getFilepath.js';
 
 nock.disableNetConnect();
 
 let tempDir;
 
 beforeEach(async () => {
-  tempDir = await fs.mkdtemp(path
+  tempDir = await fsp.mkdtemp(path
     .join(os.tmpdir(), 'page-loader-'));
 });
 
 test('fetchData', async () => {
   const url = 'https://ru.hexlet.io/courses';
-  const expected = '<!DOCTYPE html><html><body></body></html>';
+  const filepath = getFilepath('page.html', '__fixtures__');
+  const expected = await fsp.readFile(filepath, 'utf-8');
+
+  const filepathLogo = getFilepath('logo.png', '__fixtures__');
+  const expectedLogo = await fsp.readFile(filepathLogo);
+  const filepathNodejs = getFilepath('nodejs.png', '__fixtures__');
+  const expectedNodejs = await fsp.readFile(filepathNodejs);
 
   nock('https://ru.hexlet.io')
     .get('/courses')
-    .reply(200, expected);
+    .reply(200, expected)
+    .get('/courses/assets/professions/logo.png')
+    .reply(200, expectedLogo)
+    .get('/courses/assets/professions/nodejs.png')
+    .reply(200, expectedNodejs);
 
-  const [, [filename]] = await Promise.all([
-    await pageLoader(url, tempDir),
-    await fs.readdir(tempDir),
-  ]);
+  const dirContents = await pageLoader(url, tempDir)
+    .then(() => fsp.readdir(tempDir));
 
-  const content = await fs.readFile(path.join(tempDir, filename), 'utf-8');
-
+  const regHtml = /\.html$/g;
+  const htmlFile = dirContents.find((filename) => regHtml.test(filename));
+  const content = await fsp.readFile(path.join(tempDir, htmlFile), 'utf-8');
   expect(content).toBe(expected);
+
+  const regFiles = /_files$/g;
+  const dirnameFiles = dirContents.find((filename) => regFiles.test(filename));
+
+  const dirFilesContents = await fsp.readdir(path.join(tempDir, dirnameFiles));
+
+  const includeContent1 = await fsp
+    .readFile(path.join(tempDir, dirnameFiles, dirFilesContents[0]));
+  expect(includeContent1).toEqual(expectedLogo);
+
+  const includeContent2 = await fsp
+    .readFile(path.join(tempDir, dirnameFiles, dirFilesContents[1]));
+  expect(includeContent2).toEqual(expectedNodejs);
 });
